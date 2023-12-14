554cc34eb5beec0adafb1e1ca63050a69ed5d64c
Gian Marco <gianmarcopablo588@gmail.com>
2023-12-14 01:04:49 -0500

    new

--- src/app.logic.ts
+++ src/app.logic.ts
@@ -3,12 +3,7 @@ import { yarg } from "./config/plugins/yargs.adapter";
 
 const { e: edad, p: permiso, m: mensage } = yarg;
 
-
-if (edad < permiso) {
-    console.log("No tienes permiso para acceder");
-} else {
-    console.log("Bienvenido a la disco");
-    if (mensage) {
-        console.log("Bienvenido a la disco");
-    }
-}
+const mensaje = mensage ? "Bienvenido a la disco" : "No tienes permiso para acceder";
+if (edad >= permiso) {
+    console.log(mensaje);
+} 
