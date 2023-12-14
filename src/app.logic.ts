bae7b7b120697ff9b88ee25ab09757a58d7c68dd
Gian Marco <gianmarcopablo588@gmail.com>
2023-12-14 00:31:34 -0500

    main branch

--- src/app.logic.ts
+++ src/app.logic.ts
@@ -1,34 +1,35 @@
+import { yarg } from "./config/plugins/yargs.adapter";
 import fs from "fs";
 import path from "path";
 
-let headers: string = ` 
+const { b: base, l: limit, s: show } = yarg;
+
+let headers = ` 
 =============================
         TABLA DEL 5";
 =============================\n
 `
-let base: number = 5;
-let limit: number = 10;
-const fileName: string = `tabla-${base}.txt`;
-const folderPath: string = path.join(__dirname, "outputs");
-const filePath: string = path.join(folderPath, fileName);
-let resultado: string = "";
+const fileName = `tabla-${base}.txt`;
+const folderPath = path.join(__dirname, "outputs");
+const filePath = path.join(folderPath, fileName);
+let resultado = "";
 
 for (let i = 1; i <= limit; i++) {
     resultado += `${base} x ${i} = ${base * i}\n`;
 }
 
-// Crear la carpeta 'outputs' dentro de 'src' si no existe
 if (!fs.existsSync(folderPath)) {
     fs.mkdirSync(folderPath, { recursive: true });
 }
 
 resultado = headers + resultado;
 
-// Escribir en el archivo
 fs.writeFile(filePath, resultado, (err) => {
     if (err) {
         console.error("Error al escribir en el archivo:", err);
     } else {
-        console.log(`Archivo ${fileName} creado exitosamente en la carpeta src/outputs.`);
+        if (show) {
+            console.log(resultado);
+        }
     }
 });
--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,15 @@
 import { yarg } from "./config/plugins/yargs.adapter";
+import { ServerApp } from "./presentation/server-app";
 
 
 (async () => {
     await main();
 })();
 
+
 async function main() {
-    console.log(yarg);
+
+    const { b: base, l: limit, s: showTable } = yarg;
+
+    ServerApp.run({ base, limit, showTable });
 }
